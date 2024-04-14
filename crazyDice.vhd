64a19ce5632fde228dfaa3b5ff7e289b5764a1c7
random-sir <antoni.torres@outlook.com>
2024-04-14 02:28:38 -0300

    Max done

--- crazyDice.vhd
+++ crazyDice.vhd
@@ -15,14 +15,55 @@ end entity crazyDice;
 
 architecture crazyDice_arch of crazyDice is
 
+  signal d1_alt, d2_alt, d3_alt : bit_vector(2 downto 0);
+  signal partialMax             : bit_vector(2 downto 0);
+
+  component maximoDoisDados is
+    port (
+      A, B   : in  bit_vector(2 downto 0);
+      maximo : out bit_vector(2 downto 0));
+  end component maximoDoisDados;
+
 begin  -- architecture crazyDice_arch
   SumAll <= bit_vector (unsigned(d1) + unsigned(d2) + unsigned(d3));  --Soma de
                                                                       --todos
-  with nullifier select
-    SumEnul <=                          -- Soma com nullifier
-    bit_vector (unsigned(d2) + unsigned(d3))                when "01",
-    bit_vector (unsigned(d1) + unsigned(d3))                when "10",
-    bit_vector (unsigned(d1) + unsigned(d2))                when "11",
-    bit_vector (unsigned(d1) + unsigned(d2) + unsigned(d3)) when others;
+  d1_alt <= "000" when nullifier = "01" else d1;
+  d2_alt <= "000" when nullifier = "10" else d2;
+  d3_alt <= "000" when nullifier = "11" else d3;
+
+
+  SumEnul <= bit_vector (unsigned(d1_alt) + unsigned(d2_alt) + unsigned(d3_alt));  -- Soma com nullifier
+
+  max1_2   : maximoDoisDados port map (d1_alt, d2_alt, partialMaX);
+  maxFinal : maximoDoisDados port map (partialMax, d3_alt, Max);
 
 end architecture crazyDice_arch;
+
+
+
+entity maximoDoisDados is
+
+  port (
+    A, B   : in  bit_vector(2 downto 0);   -- entradas
+    maximo : out bit_vector(2 downto 0));  -- maior entre os dois
+
+end entity maximoDoisDados;
+
+architecture maximoArch of maximoDoisDados is
+  signal xor_vector : bit_vector(2 downto 0);
+  signal sel        : bit;
+  signal AorB       : bit;
+begin  -- architecture maximoArch
+
+  xor_vector <= A xor B;
+
+  with xor_vector select
+    sel <=
+    B(2) when "111"|"110"|"100"|"101",
+    B(1) when "010"|"011",
+    B(0) when "001",
+    '0'  when others;
+
+  maximo <= A when sel = '0' else B;
+
+end architecture maximoArch;
